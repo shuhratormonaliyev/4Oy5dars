54e883ad0a2444cf6657f2de4628e652e4e74f6d
shuhratormonaliyev <shuratormonaliyev7@gmail.com>
2024-05-27 00:19:09 +0500

    booline

---
+++ js/index.js
@@ -0,0 +1,236 @@
+// 1 - misol
+// let = prompt('1 dan n gacha bolgan sonni topmiz');
+// let n = +prompt('n ni kiriting');
+// let sum = 0;
+// for (let i = 1 ; i <= n; i++) {
+//     console.log(i);
+// }
+
+// 2 - misol
+
+// let = prompt("n dan 1 gacha bolgan sonni topmiz");
+// let n = +prompt("n ni kiriting");
+// let sum = n;
+// for (let i = n; i >= 1; i--) {
+//   console.log(i);
+// }
+
+// 3 - misol
+
+// let  = prompt('1 dan n gacha bolgan sonning yigindisi');
+
+// let n = +prompt('n ni kiriting');
+
+// let sum = 0;
+
+// for (let i = 1 ; i <= n; i+=1) {
+//   sum = sum + i;
+//   console.log(sum);
+// }
+
+// 4 - misol
+
+// let = prompt('1 dan n gacha bolgan sonni juftlari yigindisi');
+// let n = +prompt('n ni kiritish');
+// let sum  = 0;
+// for(i = 2; i <= n; i++){
+//   if(i % 2 == 0) {
+//   sum = sum + i;
+//   console.log(sum);
+//   }
+// }
+
+// 5 - misol
+
+// let = prompt('1 dam n gacha toqlarini chiqarish va yigindisi');
+// let n = +prompt('n ni kiriting');
+
+// let sum  = 0;
+// for(let i = 1; i <= n; i++){
+//   if(i % 2 == 1) {
+//   sum = sum + i;
+//   console.log(sum);
+//   }
+// }
+
+//  6 - misol
+
+// let = prompt('sonning boluvchilarini topish');
+// let n = +prompt('n ni kiriting');
+
+// let m = 0;
+// for (let i = 1; i <= n; i++) {
+//     if(n % i == 0){
+//       m = m + i;
+//       console.log(i);
+
+//     }
+
+// }
+
+// 7 -misol
+
+// let = prompt('sonning boluvchilarini sonini va yigindisi topish');
+// let n = +prompt('n ni kiriting');
+
+// let m = 0;
+// for (let i = 1; i <= n; i++) {
+//     if(n % i == 0){
+//       m = m + i;
+//       console.log(i);
+
+//     }console.log(m);
+
+// }
+
+// 8 - misol
+
+// let = prompt("sonning tub yoki tub emaslikka aniqlash");
+// let n = +prompt("sonni kiriting");
+// let max = 0;
+// for (let i = 1; i <= n; i++) {
+//   if (n % i == 0) {
+//     max = max + 1;
+//     console.log(i);
+//   } if(max === 2) {
+//     console.log(max);
+//   }
+//   }
+
+
+//  for -1 
+
+// let k = 'Salom'; 
+// let n = 5;
+
+// for (let i = 0; i < n; i++) {
+//     console.log(k);
+// }
+
+
+// for-2 
+
+// let = prompt('a dan b gacha bolgan sonni va ular orasidagi sonlarni ham ekranga chiqarish');
+
+// let a = +prompt('a ni kiriting a > 0');
+
+// let b = +prompt('b ni kiriting');
+
+// for(let i = a; i <= b; i++) {
+// console.log(i);
+// }
+
+
+
+// for - 3 
+
+// let = prompt('a dan b gacha bolgan sonlarni kamayish tartibda a va b dan tashqari sonlarni ekranga chiqarish');
+
+// let a = +prompt('a ni kiriting a > 0');
+
+// let b = +prompt('b ni kiriting');
+
+// for(let i = a - 1; i > b; i--) {
+// console.log(i);
+// }
+
+//  for - 4 
+// let i = +prompt('nechi kg konfet olasz');
+// let sum = 10.000; 
+
+// for(let i = 1; i <= 10; i++) {
+//   jami = sum * i;
+// }console.log(i + sum + jami + "som"); 
+
+// for - 5 
+
+// let i = +prompt('nechi kg konfet olasz');
+// let sum = 10.000; 
+
+// for(let i = 0.1; i <= 1; i++) {
+//   jami = sum * i;
+// }console.log(i + sum + jami + "som"); 
+
+//  for - 6 
+
+// let i = +prompt('nechi kg konfet olasz');
+// let sum = 10.000; 
+
+// for(let i = 1.2; i <= 2; i+=0.2) {
+//   jami = sum * i;
+// }console.log(i + sum + jami + "som"); 
+
+//  for - 7 
+
+// let  = prompt('a dan b gacha bolgan sonning yigindisi topamiz a < b');
+
+// let a = +prompt('a ni kiriting');
+// let b = +prompt('b ni kiriting');
+
+// let sum = 0;
+
+// for (let i = a ; i <= b; i+=1) {
+//   sum = sum + i;
+// }console.log(sum);
+
+
+
+//  for - 8 
+
+// let  = prompt('a dan b gacha bolgan sonning yigindisi topamiz a < b');
+
+// let a = +prompt('a ni kiriting');
+// let b = +prompt('b ni kiriting');
+
+// let sum = 1;
+
+// for (let i = a ; i <= b; i+=1) {
+//   sum = sum * i;
+// }console.log(sum);
+
+
+
+//  for - 9 misol 
+
+// let  = prompt('a dan b gacha bolgan barcha butun sonlarning kvadratlarining yigindisini topamiz a < b');
+
+// let a = +prompt('a ni kiriting');
+// let b = +prompt('b ni kiriting');
+
+// let sum = 0;
+
+// for (let i = a ; i <= b; i+=1) {
+//   sum = sum + i**2;
+// }console.log(sum);
+
+
+
+// for - 10 
+
+// let n = +prompt('n ni kiriting');
+// let S = 0; 
+// for (let i = 1; i <= n; i++) {
+//     S += 1 / i;
+// }console.log(S);
+
+
+ 
+// for - 11 
+
+// let n = +prompt('n ni kiriting');
+
+// let S = 0; 
+
+// for (let i = n; i <= 2 * n; i++) {
+//     S += i * i; 
+// }console.log(S);
+
+
+
+//  for - 12 
+
+// let n = +prompt('n ni kiriting');
+// let S = 1; 
+// for (let i = 1; i <= n; i++) {
+//     S *= 1 + i / 10;
+// }console.log(S);
